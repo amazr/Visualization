5e067dc616bbc6530c38fda3391b3c0d7da37564
AlexLaptop <alma9011@colorado.edu>
2019-09-13 15:52:17 -0600

    Playing around with displaying node id's

--- js/index.js
+++ js/index.js
@@ -1,13 +1,38 @@
+let toJson = function(res) { 
+    return res.json(); 
+};
+
 let cy = cytoscape({
     container: document.getElementById('cy'),
-    elements: [
-      { data: { id: 'a' } },
-      { data: { id: 'b' } },
-      {
-        data: {
-          id: 'ab',
-          source: 'a',
-          target: 'b'
+    
+    elements: {
+        nodes: [
+          {
+            data: { id: 'a' }
+          },
+    
+          {
+            data: { id: 'b' }
+          }
+        ],
+        edges: [
+          {
+            data: { id: 'ab', source: 'a', target: 'b' }
+          }
+        ]
+    },
+
+    layout: {
+        name: 'grid',
+        columns: 4
+    },
+
+    style: [
+        {
+            selector: 'node',
+            style: {
+                label: 'data(id)'
+            }
         }
-      }]
+    ]
   });
